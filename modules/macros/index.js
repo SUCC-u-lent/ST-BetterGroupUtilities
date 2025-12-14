6349dbbbb45f64bff1a99cc7ee3f5b644bbea99b
Banana The Almighty <william.i.ney@gmail.com>
2025-12-14 23:58:29 +1000

    Update

--- modules/macros/index.js
+++ modules/macros/index.js
@@ -1,12 +1,11 @@
 import { MacrosParser } from "/scripts/macros.js";
-import { getCurrentChatDetails } from '/script.js';
+import { chat } from '/script.js';
 
 const getMacroValue = () => {
     return "Example Value";
 };
 jQuery(async () => {
-    const context = getCurrentChatDetails();
-    console.log(context);
+    console.log(chat);
     MacrosParser.registerMacro("chargroup", ()=>getMacroValue(), "This macro is replaced by a list of all characters within the group");
 });
 
