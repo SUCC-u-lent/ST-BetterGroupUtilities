c04e60c9e188b56f2d8cc5c9e32e458aca9257e9
Banana The Almighty <william.i.ney@gmail.com>
2025-12-14 21:56:45 +1000

    Update

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-var Modules = {}
+const extensionName = "st-extension-example";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+
+var ModulePaths = {
+
+}
+var Modules = [];
+// Modules each require an index.js file that exports their public API
+ModulePaths.forEach(path => {
+    import(path + '/index.js').then(module => {
+        Modules.push(module);
+    });
+});
+
+// Helper function to create setting UI elements
+function createSettingElement(settingKey, settingValue, module) {
+    const container = $('<div class="setting-item"></div>');
+    const label = $('<label></label>').text(settingKey);
+    
+    let input;
+    
+    // Handle different types of settings
+    if (typeof settingValue === 'boolean') {
+        input = $('<input type="checkbox">')
+            .prop('checked', settingValue)
+            .on('change', function() {
+                module.settings[settingKey] = $(this).prop('checked');
+                // Save settings if module has a save function
+                if (module.saveSettings) {
+                    module.saveSettings();
+                }
+            });
+    } else if (typeof settingValue === 'number') {
+        input = $('<input type="number">')
+            .val(settingValue)
+            .on('input', function() {
+                module.settings[settingKey] = parseFloat($(this).val());
+                if (module.saveSettings) {
+                    module.saveSettings();
+                }
+            });
+    } else {
+        input = $('<input type="text">')
+            .val(settingValue)
+            .on('input', function() {
+                module.settings[settingKey] = $(this).val();
+                if (module.saveSettings) {
+                    module.saveSettings();
+                }
+            });
+    }
+    
+    container.append(label).append(input);
+    return container;
+}
+
+jQuery(async ()=>{
+    const settingsHtml = await $.get(`${extensionFolderPath}/mainHtml.html`);
+    $('#extension-settings').append(settingsHtml);
+    const moduleContainerTemplate = $(settingsHtml).find('#module-container-template')
+    
+    Modules.forEach(module=>{
+        if (module.settings){
+            // Clone the template for this module
+            const moduleContainer = moduleContainerTemplate.clone(true, true);
+            moduleContainer.removeAttr('id'); // Remove template id
+            moduleContainer.removeAttr('hidden'); // Make it visible
+            
+            // Set module title if provided
+            if (module.name) {
+                moduleContainer.find('.inline-drawer-toggle b').text(module.name);
+            }
+            
+            const contentArea = moduleContainer.find('.inline-drawer-content');
+            
+            // Generate settings UI for each setting
+            Object.keys(module.settings).forEach(settingKey => {
+                const settingValue = module.settings[settingKey];
+                const settingElement = createSettingElement(settingKey, settingValue, module);
+                contentArea.append(settingElement);
+            });
+            
+            // Append the module container to the main settings
+            $('#extension-settings').append(moduleContainer);
+        }
+    })
+})
