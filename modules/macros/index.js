aeeac4a4351ca851af2cbf8e8c5f3c02b2fe7bf5
Banana The Almighty <william.i.ney@gmail.com>
2025-12-14 23:15:33 +1000

    Update

--- modules/macros/index.js
+++ modules/macros/index.js
@@ -1,3 +1,5 @@
+import { SlashCommandParser, SlashCommand, SlashCommandNamedArgument } from "../../../slash-commands.js";
+
 export default {
     name: "Macros Module",
     settings: {
@@ -5,3 +7,26 @@ export default {
     },
     cannotBeDisabled: true
 }
+
+jQuery(async()=>{
+    SlashCommandParser.addCommandObject(
+        SlashCommand.fromProps({
+            name: "hello",            // `/hello`
+            callback: (namedArgs, unnamedArgs) => {
+                // namedArgs: object of `--option=value` flags
+                // unnamedArgs: remainder string
+                return `Hi there ${unnamedArgs || ""}`;
+            },
+            helpString: "Say hello with optional text",
+            aliases: ["hi"],          // `/hi` also works
+            // You can define arguments if you need them:
+            namedArgumentList: [
+                SlashCommandNamedArgument.fromProps({
+                    name: "times",
+                    description: "How many times to repeat greeting",
+                    defaultValue: "1"
+                })
+            ]
+        })
+    );
+})
